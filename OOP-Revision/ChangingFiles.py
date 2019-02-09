b0100998e2ed7d219e7b9b055821aa5bf7ca92e0
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2019-02-09 22:19:46 +0200

    fixing an error in renaming

--- OOP-Revision/ChangingFiles.py
+++ OOP-Revision/ChangingFiles.py
@@ -1,14 +1,20 @@
 import os
 
-class FileNameChanger:
+class fileNameChanger:
     i = 1
-    def changer(self, dirName, obj):
-        for fileName in os.listdir(dirName ):
-            desired = obj + str(i) + '.txt'
-            src = dirName + fileName
-            desired = dirName + desired
+    def changer(self, dir, pre):
+        i = 1
+        for fileName in os.listdir(dir):
+            dis = dir + "/" + pre + str(i) + ".txt"
+            src = dir + "/" + fileName
+            dis = dir + "/" + dis
+            i += 1
+            os.rename(src, dis)
 
-            os.rename(src, desired)
 
-        i += 1
 
+if __name__ == '__main__':
+    t = fileNameChanger()
+    des = input("Enter the path of the directory of the files: ")
+    prefix = input("Enter The required prefix to change the names according to: ")
+    t.changer(des, prefix)
